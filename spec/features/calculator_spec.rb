c3e0b86c4646c9303fe38ab9a21b44a8a3d8361c
Gabriel Rascovit <rascovit@gmail.com>
2017-10-27 10:11:53 -0200

    create more feature specs

--- spec/features/calculator_spec.rb
+++ spec/features/calculator_spec.rb
@@ -21,8 +21,8 @@ feature 'Calculator', type: :feature do
       fill_in 'address', with: 'Goiânia'
 
       expect(page).to have_selector('.pac-item')
-      
-      find('#address').send_keys(:down)      
+
+      find('#address').send_keys(:down)
       find('#address').send_keys(:enter)
 
       expect(page).to have_field('address', with: 'Goiânia - State of Goiás, Brazil')
@@ -36,11 +36,96 @@ feature 'Calculator', type: :feature do
 
       expect(page).to have_selector('.pac-item')
 
-      find('#address').send_keys(:down)      
+      find('#address').send_keys(:down)
       find('#address').send_keys(:enter)
 
       expect(page).to have_selector("#latitude[value='-16.68689119999999']", visible: false)
       expect(page).to have_selector("#longitude[value='-49.264794300000005']", visible: false)
+    end
+
+    it 'redirects to result page after form submit', js: true do
+      visit root_path
+
+      fill_in 'long_term_income', with: '500'
+      fill_in 'address', with: 'Goiânia'
+
+      expect(page).to have_selector('.pac-item')
+
+      find('#address').send_keys(:down)
+      find('#address').send_keys(:enter)
+      click_button('Calculate')
+
+      expect(page.current_path).to eq('/calculator')
+    end
+  end
+
+  describe 'result page' do
+    it 'displays 3 paragraphs containing the result data', js: true do
+      visit root_path
+
+      fill_in 'long_term_income', with: '500'
+      fill_in 'address', with: 'Goiânia'
+
+      expect(page).to have_selector('.pac-item')
+
+      find('#address').send_keys(:down)
+      find('#address').send_keys(:enter)
+      click_button('Calculate')
+
+      expect(page).to have_selector('p', count: 3)
+    end
+
+    it 'displays a back to the calculator button', js: true do
+      visit root_path
+
+      fill_in 'long_term_income', with: '500'
+      fill_in 'address', with: 'Goiânia'
+
+      expect(page).to have_selector('.pac-item')
+
+      find('#address').send_keys(:down)
+      find('#address').send_keys(:enter)
+      click_button('Calculate')
+
+      expect(page).to have_link('Go back to the calculator', href: '/calculator/new')
+    end
+
+    context 'when airbnb is more profitable' do
+      it 'displays the desired result data', js: true do
+        visit root_path
+
+        fill_in 'long_term_income', with: '10'
+        fill_in 'address', with: 'Goiânia'
+
+        expect(page).to have_selector('.pac-item')
+
+        find('#address').send_keys(:down)
+        find('#address').send_keys(:enter)
+        click_button('Calculate')
+
+        expect(page).to have_css('p', text: /You could earn an average of \$\d+\.\d+ per month with Airbnb./)
+        expect(page).to have_css('p', text: /You could earn \$\d+\.\d+ \(\d+.\d+%\) more than long term rent./)
+        expect(page).to have_css('p', text: /It is profitable to do Airbnb./)
+      end
+    end
+
+    context 'when long term rent is more profitable' do
+      it 'displays the desired result data', js: true do
+        visit root_path
+
+        fill_in 'long_term_income', with: '10000'
+        fill_in 'address', with: 'Goiânia'
+
+        expect(page).to have_selector('.pac-item')
+
+        find('#address').send_keys(:down)
+        find('#address').send_keys(:enter)
+        click_button('Calculate')
+
+        expect(page).to have_css('p', text: /You could earn an average of \$\d+\.\d+ per month with Airbnb./)
+        expect(page).to have_css('p', text: /You could earn \$\d+\.\d+ \(\-\d+.\d+%\) less than long term rent./)
+        expect(page).to have_css('p', text: /It is not profitable to do Airbnb./)
+      end
     end
   end
 end
